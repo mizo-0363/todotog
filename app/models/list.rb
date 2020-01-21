c3cbe1af8c3faf9c927ea5ed3873a6389a0a50bd
mizo-0363 <gugu.net1977@gmail.com>
2020-01-21 16:46:03 +0900

    commit

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
 
-  validates :title, length: { in: 1..255}
+  validates :title, length: { in: 1..255 }
 
   belongs_to :user
+  has_many :cards, dependent: :destroy
 
 end
